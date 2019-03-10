b1f8bbae6a043a2f3b6a3c41cd77c22cf8a4d44d
OlkaB <aleksandra.barecka@code4.pl>
2019-03-10 21:10:19 +0100

    scoped slots

---
+++ src/components/ScopedSlot.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <h4>Items list</h4>
+    <ul class="list-item" v-for="shape in shapes" :key="shape.name">
+      <slot :shape="shape"></slot>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      shapes: [
+        { name: "Square", sides: 4 },
+        { name: "Hexagon", sides: 6 },
+        { name: "Triangle", sides: 3 }
+      ]
+    };
+  }
+};
+</script>
+
--- src/views/ScopedSlots.vue
+++ src/views/ScopedSlots.vue
@@ -1,5 +1,21 @@
 <template>
     <div class="scopedSlots">
         <h3>scopedSlots</h3>
+        <ScopedSlotCmp>
+            <template slot-scope="{shape}">
+                {{ shape.name }}
+                <small>({{ shape.sides }} sides)</small>
+            </template>
+        </ScopedSlotCmp>
     </div>
 </template>
+
+<script>
+import ScopedSlotCmp from '@/components/ScopedSlot.vue'
+
+export default {
+    components: {
+        ScopedSlotCmp
+    },
+}
+</script>
